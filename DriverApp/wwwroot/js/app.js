1d475599a2d9540e74a216fda674880871266013
savigun <georgedragos99@gmail.com>
2017-07-27 16:13:40 +0300

    a

---
+++ DriverApp/wwwroot/js/app.js
@@ -0,0 +1,32 @@
+﻿(function () {
+
+	var app = angular.module("app", ["ngRoute"]);
+
+	app.config(function ($httpProvider, $routeProvider, $locationProvider) {
+		$httpProvider.defaults.withCredentials = true;
+
+		$locationProvider.html5Mode(true);
+		$routeProvider
+			.when("/", {
+				templateUrl: "views/drivers.html"
+			})
+			.when("/drivers", {
+				templateUrl: "views/drivers.html"
+			})
+			.when("/orders", {
+				templateUrl: "views/orders.html"
+			})
+			.when("/trips", {
+				templateUrl: "views/trips.html"
+			});
+	});
+
+	app.run(function ($rootScope, Api, Auth) {
+		$rootScope.Auth = Auth;
+
+		Api.checkSession().then(function () {
+			Auth.authenticated = true;
+		});
+	});
+
+})();
