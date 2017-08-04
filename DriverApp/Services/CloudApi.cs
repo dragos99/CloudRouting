43bef032eea65d30150319b7fb517c0d91777e99
savigun <georgedragos99@gmail.com>
2017-08-04 12:51:42 +0300

    a

--- DriverApp/Services/CloudApi.cs
+++ DriverApp/Services/CloudApi.cs
@@ -12,8 +12,8 @@ using System.Threading.Tasks;
 
 namespace DriverApp.Services
 {
-    public class CloudApi
-    {
+	public class CloudApi
+	{
 		private ILogger _logger;
 		private string _key = "311A1B20704D4EA797EE6E9B1D618999";
 		private string _routingProfile = "R-EUR-001";
@@ -27,13 +27,13 @@ namespace DriverApp.Services
 		}
 
 
-        public async Task<TriggerResponse> TriggerRouting(IEnumerable<Order> orders)
-        {
+		public async Task<TriggerResponse> TriggerRouting(IEnumerable<Order> orders)
+		{
 
-            TriggerResponse trip = null;
+			TriggerResponse trip = null;
 
-            try
-            {
+			try
+			{
 				TriggerRequest triggerRequest = new TriggerRequest
 				{
 					RequestReference = 1,
@@ -46,10 +46,11 @@ namespace DriverApp.Services
 					}
 				};
 
-                foreach (var order in orders)
-                {
-                    triggerRequest.Data.Addresses.Add(new Address { Lat = order.GeoX, Long = order.GeoY, Id = order.Id.ToString() });
-                    triggerRequest.Data.Orders.Add(new RequestOrder {
+				foreach (var order in orders)
+				{
+					triggerRequest.Data.Addresses.Add(new Address { Lat = order.GeoX, Long = order.GeoY, Id = order.Id.ToString() });
+					triggerRequest.Data.Orders.Add(new RequestOrder
+					{
 						TimeWindowTill = order.TimeWindowTill,
 						TimeWindowFrom = order.TimeWindowFrom,
 						FixedDurationInSec = order.FixedDurationInSec,
@@ -57,20 +58,20 @@ namespace DriverApp.Services
 						Type = order.OrderType,
 						Id = Int32.Parse(order.OrderNumber)
 					});
-                }
+				}
 
-                StringContent content = new StringContent(JsonConvert.SerializeObject(triggerRequest), Encoding.UTF8, "application/json");
-                var response = await _client.PostAsync($"/api/v1/routing?key={_key}&profile={_routingProfile}&async=false", content);
-                trip = JsonConvert.DeserializeObject<TriggerResponse>(response.Content.ReadAsStringAsync().Result);
-            }
-            catch (HttpRequestException httpRequestException)
-            {
-                _logger.LogInformation($"Error getting route from OrtecCloud: {httpRequestException.Message}");
-            }
+				StringContent content = new StringContent(JsonConvert.SerializeObject(triggerRequest), Encoding.UTF8, "application/json");
+				var response = await _client.PostAsync($"/api/v1/routing?key={_key}&profile={_routingProfile}&async=false", content);
+				trip = JsonConvert.DeserializeObject<TriggerResponse>(response.Content.ReadAsStringAsync().Result);
+			}
+			catch (HttpRequestException httpRequestException)
+			{
+				_logger.LogInformation($"Error getting route from OrtecCloud: {httpRequestException.Message}");
+			}
 
-            return trip;
-        }
+			return trip;
+		}
 
-
+	}
         
 }
